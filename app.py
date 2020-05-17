1d25e20cf6ecf08a82f1c3a86c7adfd75d920807
jgoffin <jacobgoffin@gmail.com>
2020-05-17 12:42:03 -0700

    using timepicker, added logo

--- app.py
+++ app.py
@@ -24,17 +24,38 @@ def home_endpoint():
 @app.route('/predict', methods=['POST'])
 def get_prediction():
     # Works only for a single sample
-    print(request)
     if request.method == 'POST':
         data = request.form
+        print(data)
+        bike_split = map(int, data['bike'].split(':'))
+        # Sum hour, minute, seconds into a single seconds value
+        bike_time_secs = sum([a*b for a, b in zip(bike_split, [3600, 60, 1])])
+
+        run_split = map(int, data['run'].split(':'))
+        # Sum hour, minute, seconds into a single seconds value
+        run_time_secs = sum([a * b for a, b in zip(run_split, [3600, 60, 1])])
+
+        if data['t1']:
+            t1_split = map(int, data['t1'].split(':'))
+            # Sum minute, seconds into a single seconds value
+            t1_time_secs = sum([a * b for a, b in zip(t1_split, [ 60, 1])])
+            t1_time_secs = None if data['t1']=="00:00" else t1_time_secs
+
+        if data['t2']:
+            t2_split = map(int, data['t2'].split(':'))
+            # Sum minute, seconds into a single seconds value
+            t2_time_secs = sum([a * b for a, b in zip(t2_split, [60, 1])])
+            t2_time_secs = None if data['t2']=="00:00" else t2_time_secs
+
+
         data_dict = {
-            'bike_time_secs': int(data['bike']),
-            'run_time_secs': int(data['run']),
+            'bike_time_secs': bike_time_secs,
+            'run_time_secs': run_time_secs,
             'age': int(data['age']) if data['age'] else None,
             'gender': data.get('gender',None),
             'age_flag': 1 if data['age'] else 0,
-            't1_secs': int(data['t1']) if data['t1'] else None,
-            't2_secs': int(data['t2']) if data['t2'] else None,
+            't1_secs': t1_time_secs if data['t1'] else None,
+            't2_secs': t2_time_secs if data['t2'] else None,
             't1_flag': 1 if data['t1'] else 0,
             't2_flag': 1 if data['t2'] else 0,
         }
--- static/form.js
+++ static/form.js
@@ -12,7 +12,40 @@ $("#myForm").submit(function(e) {
            data: form.serialize(), // serializes the form's elements.
            success: function(data)
            {
-               $("#message").text("Predicted Swim Time: "+data+" (seconds)");
+               console.log(data);
+               $("#prediction").show();
+               hours = Math.floor(data/3600);
+               minutes = Math.floor((data - 3600*hours)/60);
+               seconds = Math.round(data - 3600*hours - 60*minutes)
+               $("#message").text(hours + " hours, "+minutes+" minutes, and "+seconds+" seconds");
+               $(window).scrollTop($('#prediction').offset().top)
+           },
+           error: function(jqXHR, textStatus, errorThrown)
+           {
+                alert(errorThrown);
            }
          });
+});
+
+$('.timepicker1').timepicker({
+    timeFormat: 'HH:mm:ss',
+    interval: 60,
+    minTime: '00:00:00',
+    maxTime: '3:00:00',
+    defaultTime: '00:00:00',
+    startTime: '00:00:00',
+    dynamic: false,
+    dropdown: false,
+    scrollbar: false
+});
+$('.timepicker2').timepicker({
+    timeFormat: 'mm:ss',
+    interval: 0,
+    minTime: '00:00',
+    maxTime: '30:00',
+    defaultTime: '00:00',
+    startTime: '00:00',
+    dynamic: false,
+    dropdown: false,
+    scrollbar: false
 });
